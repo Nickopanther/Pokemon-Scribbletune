0b961fd03e52bd32af8561fd0406f09333829588
Nicholas Azzarello <nicko789@Melissas-MacBook-Pro.local>
2018-09-30 23:24:45 -0400

    Got rid of some extra code

--- route_1.js
+++ route_1.js
@@ -1,17 +1,22 @@
 const scribble = require('scribbletune');
 
 
-// console.log(scribble);
+/*
 
+  Pokemon Red/Blue/Yellow - Route 1
+
+*/
 
 
 var route_1 = scribble.clip({
 
-  // - <------- That dash means it is a rest beat
+/*
+    - <------- That dash means it is a rest beat
 
-  // _ <----- That means the note holds down
+    _ <----- That means the note holds down
 
-          // First Part
+*/
+     // First Part
   notes: ['d4', 'e4', 'f#4', 'f#4', 'f#4',
          'd4', 'e4', 'f#4', 'f#4', 'f#4',
          'd4', 'e4', 'f#4', 'f#4', 'g4', 'f#4', 'e4',
@@ -56,6 +61,7 @@ var route_1 = scribble.clip({
 
           'c#4', 'd4', 'e4', 'g4', 'f#4', 'e4', 'd4', 'c#4', 'b3', 'd4', 'a4', 'c#4', 'd4', 'c#3', 'd3'
 ],
+
           // First Part
   pattern: 'xxx-x-x-'
           +'xxx-x-x-'
@@ -101,151 +107,5 @@ var route_1 = scribble.clip({
 
           +'xxx-x-x-x-x-x-x-x-x___x___x_x_x__________----'
 });
+
 scribble.midi(route_1, 'route_1.midi');
-/*
-var route_1LowOctave = scribble.clip({
-
-  // - <------- That dash means it is a rest beat
-
-  // _ <----- That means the note holds down
-
-          // First Part
-  notes:  ['d3' 'e3' 'f#3' 'f#3' 'f#3'
-          'd3' 'e3' 'f#3' 'f#3' 'f#3'
-          'd3' 'e3' 'f#3' 'f#3' 'g3' 'f#3' 'e3'
-
-          'c#3' 'd3' 'e3' 'e3' 'e3'
-          'c#3' 'd3' 'e3' 'e3' 'e3'
-          'c#3' 'd3' 'e3' 'e3' 'f#3' 'e3' 'e3' 'f#3' 'd3'
-
-          'd3' 'e3' 'f#3' 'f#3' 'f#3'
-          'd3' 'e3' 'f#3' 'f#3' 'f#3'
-          'd3' 'e3' 'f#3' 'f#3' 'g3' 'f#3' 'e3'
-
-          'c#3' 'd3' 'e3' 'g3' 'f#3' 'e3' 'd3' 'c#3' 'b2' 'd3' 'a3' 'c#3' 'd3'
-
-          // Second Part
-          'f#3' 'g3' 'a3' 'a3' 'f#3' 'd3' 'd4' 'c#4' 'b3' 'c#4' 'a3' 'f#3' 'd3' 'f#3' 'e3'
-          'f#3' 'g3' 'a3' 'a3' 'f#3' 'd3' 'd4' 'c#4' 'b3' 'g3' 'a3' 'd4' 'c#4' 'e4' 'd4'
-
-        // First Part
-        'd3' 'e3' 'f#3' 'f#3' 'f#3'
-        'd3' 'e3' 'f#3' 'f#3' 'f#3'
-        'd3' 'e3' 'f#3' 'f#3' 'g3' 'f#3' 'e3'
-
-        'c#3' 'd3' 'e3' 'e3' 'e3'
-        'c#3' 'd3' 'e3' 'e3' 'e3'
-        'c#3' 'd3' 'e3' 'e3' 'f#3' 'e3' 'e3' 'f#3' 'd3'
-
-        'd3' 'e3' 'f#3' 'f#3' 'f#3'
-        'd3' 'e3' 'f#3' 'f#3' 'f#3'
-        'd3' 'e3' 'f#3' 'f#3' 'g3' 'f#3' 'e3'
-
-        'c#3' 'd3' 'e3' 'g3' 'f#3' 'e3' 'd3' 'c#3' 'b2' 'd3' 'a3' 'c#3' 'd3'
-
-        // Second Part
-        'f#3' 'g3' 'a3' 'a3' 'f#3' 'd3' 'd4' 'c#4' 'b3' 'c#4' 'a3' 'f#3' 'd3' 'f#3' 'e3'
-        'f#3' 'g3' 'a3' 'a3' 'f#3' 'd3' 'd4' 'c#4' 'b3' 'g3' 'a3' 'd4' 'c#4' 'e4' 'd4'
-
-        // Ending
-        'd3' 'e3' 'f#3' 'f#3' 'f#3'
-        'd3' 'e3' 'f#3' 'f#3' 'f#3'
-        'd3' 'e3' 'f#3' 'f#3' 'g3' 'f#3' 'e3'
-
-        'c#3' 'd3' 'e3' 'g3' 'f#3' 'e3' 'd3' 'c#3' 'b2' 'd3' 'a3' 'c#3' 'd3' 'c#2' 'd2'
-],
-
-          // First Part
-  pattern: 'xxx-x-x-'
-          +'xxx-x-x-'
-          +'xxx-x-x__xx___-'
-
-          +'xxx-x-x-'
-          +'xxx-x-x-'
-          +'xxx-x-xxxxx___-'
-
-          +'xxx-x-x-'
-          +'xxx-x-x-'
-          +'xxx-x-x__xx___-'
-
-          +'xxx-x-x-x-x-x-x-x-x___x___x___-'
-
-          // Second Part
-          +'xxx-x-x-x-x-x-x-x-x-x-x__xx____'
-          +'xxx-x-x-x-x-x-x-x-x-x-x-x-x____-'
-
-          // First Part
-          +'xxx-x-x-'
-          +'xxx-x-x-'
-          +'xxx-x-x__xx___-'
-
-          +'xxx-x-x-'
-          +'xxx-x-x-'
-          +'xxx-x-xxxxx___-'
-
-          +'xxx-x-x-'
-          +'xxx-x-x-'
-          +'xxx-x-x__xx___-'
-
-          +'xxx-x-x-x-x-x-x-x-x___x___x___-'
-
-          // Second Part
-          +'xxx-x-x-x-x-x-x-x-x-x-x__xx____'
-          +'xxx-x-x-x-x-x-x-x-x-x-x-x-x____-'
-
-          // Ending
-          +'xxx-x-x-'
-          +'xxx-x-x-'
-          +'xxx-x-x__xx___-'
-
-          +'xxx-x-x-x-x-x-x-x-x___x___x_x_x__________----'
-});
-*/
-
-/*
-var route_1LowOctaveTwo = scribble.clip({
-
-  // First Part
-  notes: ['d3', 'e3', 'f#3', 'f#3', 'f#3',
-  'd3', 'e3', 'f#3', 'f#3', 'f#3',
-  'd3', 'e3', 'f#3', 'f#3', 'g3', 'f#3', 'e3',
-
-  'c#3', 'd3', 'e3', 'e3', 'e3',
-  'c#3', 'd3', 'e3', 'e3', 'e3',
-  'c#3', 'd3', 'e3', 'e3', 'f#3', 'e3', 'e3', 'f#3', 'd3', 'f#3',
-
-  'd3', 'e3', 'f#3', 'f#3', 'f#3',
-  'd3', 'e3', 'f#3', 'f#3', 'f#3',
-  'd3', 'e3', 'f#3', 'f#3', 'g3', 'f#3', 'e3',
-
-  'c#3', 'd3', 'e3', 'g3', 'f#3', 'e3', 'd3', 'c#3', 'b2', 'd3', 'a3', 'c#3', 'd3',
-
-  // Second Part
-  'f#3', 'g3', 'a3', 'a3', 'f#3', 'd3', 'd4', 'c#4', 'b3', 'c#4', 'a3', 'f#3', 'd3', 'f#3', 'e3',
-  'f#3', 'g3', 'a3', 'a3', 'f#3', 'd3', 'd4', 'c#4', 'b3', 'g3', 'a3', 'd4', 'c#4', 'e4', 'd4'
-],
-
-        // First Part
-pattern: 'xxx-x-x-'
-        +'xxx-x-x-'
-        +'xxx-x-x__xx___-'
-
-        +'xxx-x-x-'
-        +'xxx-x-x-'
-        +'xxx-x-xxxxx___x_'
-
-        +'xxx-x-x-'
-        +'xxx-x-x-'
-        +'xxx-x-x__xx___-'
-
-        +'xxx-x-x-x-x-x-x-x-x___x___x___-'
-
-        // Second Part
-        +'xxx-x-x-x-x-x-x-x-x-x-x__xx____'
-        +'xxx-x-x-x-x-x-x-x-x-x-x-x-x____-'
-
-
-
-
-});
-*/
